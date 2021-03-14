1d9c704a5a8e30b1a401e35a5fe884398578575a
Jazmin Ferreiro <jazminferreiro@Jazmins-MacBook-Pro.local>
2021-03-13 21:04:31 -0300

    first commit

---
+++ build/esp-idf/mbedtls/mbedtls/include/cmake_install.cmake
@@ -0,0 +1,122 @@
+# Install script for directory: /Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "/Users/jazminferreiro/esp/xtensa-esp32-elf/bin/xtensa-esp32-elf-objdump")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/mbedtls" TYPE FILE PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ FILES
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/aes.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/aesni.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/arc4.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/aria.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/asn1.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/asn1write.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/base64.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/bignum.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/blowfish.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/bn_mul.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/camellia.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ccm.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/certs.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/chacha20.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/chachapoly.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/check_config.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/cipher.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/cipher_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/cmac.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/compat-1.3.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/config.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ctr_drbg.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/debug.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/des.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/dhm.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ecdh.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ecdsa.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ecjpake.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ecp.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ecp_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/entropy.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/entropy_poll.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/error.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/gcm.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/havege.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/hkdf.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/hmac_drbg.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/md.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/md2.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/md4.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/md5.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/md_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/memory_buffer_alloc.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/net.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/net_sockets.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/nist_kw.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/oid.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/padlock.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pem.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pk.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pk_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pkcs11.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pkcs12.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/pkcs5.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/platform.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/platform_time.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/platform_util.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/poly1305.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ripemd160.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/rsa.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/rsa_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/sha1.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/sha256.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/sha512.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl_cache.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl_ciphersuites.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl_cookie.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl_internal.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/ssl_ticket.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/threading.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/timing.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/version.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/x509.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/x509_crl.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/x509_crt.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/x509_csr.h"
+    "/Users/jazminferreiro/esp/esp-idf/components/mbedtls/mbedtls/include/mbedtls/xtea.h"
+    )
+endif()
+
---
+++ main/station_example_main.c
@@ -0,0 +1,234 @@
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <string.h>
+#include <esp_http_client.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+/* The examples use WiFi configuration that you can set via project configuration menu
+
+   If you'd rather not, just change the below entries to strings with
+   the config you want - ie #define EXAMPLE_WIFI_SSID "mywifissid"
+*/
+#define EXAMPLE_ESP_WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define EXAMPLE_ESP_WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+#define EXAMPLE_ESP_MAXIMUM_RETRY  CONFIG_ESP_MAXIMUM_RETRY
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+static const char *TAG = "wifi station";
+
+static int s_retry_num = 0;
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt){
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER");
+            printf("%.*s", evt->data_len, (char*)evt->data);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                printf("%.*s", evt->data_len, (char*)evt->data);
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            break;
+    }
+    return ESP_OK;
+}
+
+static void http_request(void *pvParameters) {
+    ESP_LOGI(TAG, "in http request function");
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+
+    esp_http_client_config_t config = {
+            .host = "httpbin.org",
+            .path = "/get",
+            .query = "esp",
+            .event_handler = _http_event_handler,
+            .user_data = local_response_buffer,        // Pass address of local buffer to get response
+            .disable_auto_redirect = true,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+        ESP_LOGI(TAG, "response %s", local_response_buffer);
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+
+    // POST
+    const char *post_data = "{\"field1\":\"value1\"}";
+    esp_http_client_set_url(client, "http://httpbin.org/post");
+    esp_http_client_set_method(client, HTTP_METHOD_POST);
+    esp_http_client_set_header(client, "Content-Type", "application/json");
+    esp_http_client_set_post_field(client, post_data, strlen(post_data));
+    err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+    }
+
+    esp_http_client_cleanup(client);
+    ESP_LOGI(TAG, "Finish http example");
+    vTaskDelete(NULL);
+}
+
+
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(void)
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = EXAMPLE_ESP_WIFI_SSID,
+            .password = EXAMPLE_ESP_WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+
+void app_main(void)
+{
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+    wifi_init_sta();
+    xTaskCreate(&http_request, "http_request", 8192, NULL, 5, NULL);
+}
